b698185f615ae3c1798ffd08cf16ae99405d98a3
taylor.wtf <109391572+taylorwtf@users.noreply.github.com>
2023-05-24 19:31:32 -0700

    added provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,11 @@
 // pages/api/auth/[...nextauth].js
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
     // Add your desired authentication providers here
-    Providers.Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
